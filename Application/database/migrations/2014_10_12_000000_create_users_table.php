3e57df6fb6ba7b58f562899664de487d6dcacfd2
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-08-16 20:07:23 +0600

    user table
    user table in database

--- Application/database/migrations/2014_10_12_000000_create_users_table.php
+++ Application/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('full_name');
+            $table->string('username')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->enum('role',['admin','vendor','customer'])->default('customer');
+             $table->enum('status',['active','inactive'])->default('active');
             $table->rememberToken();
             $table->timestamps();
         });
