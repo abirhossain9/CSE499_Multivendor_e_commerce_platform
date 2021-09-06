a2b1897499e6535243e3a5b66f3d68ce9015a410
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-06 20:46:42 +0600

    employee model updated #8
    employee model updated

--- application/app/Models/Backend/Employee.php
+++ application/app/Models/Backend/Employee.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'fullname',
+        'designation',
+        'slug',
+        'overview',
+        'phone',
+        'address',
+        'email',
+        'profile_pic',
+        'status'
+    ];
 }
