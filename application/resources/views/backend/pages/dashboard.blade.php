d93634d455674de72ce6841559aad423968a03b1
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-09-07 16:24:40 +0600

    icon change for employee #17
    icon change for employee

--- application/resources/views/backend/pages/dashboard.blade.php
+++ application/resources/views/backend/pages/dashboard.blade.php
@@ -27,7 +27,7 @@
           <div class="col-sm-6 col-xl-3 mg-t-20 mg-sm-t-0">
             <div class="bg-purple rounded overflow-hidden">
               <div class="pd-x-20 pd-t-20 d-flex align-items-center">
-                <i class="ion ion-bag tx-60 lh-0 tx-white op-7"></i>
+                <i class="fas fa-user tx-60 lh-0 tx-white op-7"></i>
                 <div class="mg-l-20">
                   <p class="tx-10 tx-spacing-1 tx-mont tx-semibold tx-uppercase tx-white-8 mg-b-10">Total Employees</p>
                   <p class="tx-24 tx-white tx-lato tx-bold mg-b-0 lh-1">{{$employees->count()}}</p>
