d2e527633df6d08dd149ecf7ed45a44dc9db72bd
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-10-09 21:28:45 +0600

    Added JS for Deal of the Week
    Added JS for Deal of the Week timer.

--- frontendUI/js/custom.js
+++ frontendUI/js/custom.js
@@ -28,13 +28,63 @@ jQuery(document).ready(function ($)
 
     initFavorite();
     initIsotopeFiltering();
+    initTimer();
 
 // 2.  Inits Menu
 
 
 // 3.  Init Timer
 
+function initTimer()
+{
+    if($('.timer').length)
+    {
+        //uncomment line below and relocate date
+        var target_date = new Date("Oct 15, 2021").getTime();
 
+
+        // comment lines below
+        // var date = new Date();
+        // date.setDate(date.getDate() + 3);
+        // var target_date = date.getTime();
+        // --------------------------------------------------------
+
+        // variables for time units
+        var days, hours, minutes, seconds;
+        var d = $('#day');
+        var h = $('#hour');
+        var m = $('#minute');
+        var s = $('#second');
+
+        setInterval(function() 
+        {
+            //find the amound of "seconds" between now and target
+
+            var current_date = new Date().getTime();
+            var seconds_left = (target_date - current_date) / 1000;
+
+            //do some calculations
+            days = parseInt(seconds_left / 86400);
+            seconds_left = seconds_left % 86400;
+
+            hours = parseInt(seconds_left / 3600);
+            seconds_left = seconds_left % 3600;
+
+            minutes = parseInt(seconds_left / 60);
+            seconds = parseInt(seconds_left % 60);
+
+            //display result
+            d.text(days);
+            h.text(hours);
+            m.text(minutes);
+            s.text(seconds);
+
+
+        
+        
+        }, 1000 );
+    }
+}
 
 // 4.  Init Favorite
 function initFavorite() 
