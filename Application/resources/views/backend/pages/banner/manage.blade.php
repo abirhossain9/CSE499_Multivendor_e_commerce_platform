eb8853da786a5cd216a93916afcb49470ed68ff7
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-10 15:40:51 +0600

    Banner Manage page #20

---
+++ Application/resources/views/backend/pages/banner/manage.blade.php
@@ -0,0 +1,126 @@
+@extends('backend.layout.template') @section('body')
+
+<div class="br-pagetitle">
+    <i class="icon ion-ios-home-outline"></i>
+    <div>
+        <h4>Manage All Banner</h4>
+        <p class="mg-b-0">Manage Banners From Here.</p>
+    </div>
+</div>
+
+<div class="br-pagebody">
+    <div class="row">
+        <div class="col-lg-12">
+            {{-- page body content start --}}
+
+            <div class="card bd-0 shadow-base">
+                <div class="d-md-flex justify-content-between pd-25">
+
+                    {{-- Table Content start --}}
+                    <div class="bd bd-gray-300 rounded table-responsive">
+                        <table class="table table-bordered table-striped table-hover table-custom">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">#sl.</th>
+                                    <th scope="col">Banner Image</th>
+                                    <th scope="col">Banner Text</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php $i = 0; @endphp
+                                @foreach($banners as $banner)
+                                @php $i++; @endphp
+
+                                <tr>
+                                    <th scope="row">{{$i;}}</th>
+                                    <td>@if ($banner->banner_image==NULL)
+                                         <img src="{{asset('backend/img/banner/default.jpg')}}" alt="" width="40">
+                                    @else
+                                         <img src="{{asset('backend/img/banner/'.$banner->banner_image)}}" alt="" width="40">
+
+                                    @endif</td>
+                                    <td>{{$banner->banner_text}}</td>
+                                    <td>
+                                        @if ($banner->status==1)
+                                        <span class="badge badge-success">active</span>
+                                        @elseif ($banner->status==2)
+                                        <span class="badge badge-danger">inactive</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <ul class="custom-action">
+                                            <li>
+                                                <a href="{{route('banner.edit',$banner->id)}}">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="" data-toggle="modal" data-target="#banners{{$banner->id}}">
+                                                    <i class="fa fa-trash "></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </td>
+                                    <!-- Modal -->
+                                    <div
+                                        class="modal fade"
+                                        id="banners{{$banner->id}}"
+                                        tabindex="-1"
+                                        role="dialog"
+                                        aria-labelledby="exampleModalCenterTitle"
+                                        aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLongTitle">Delete This banner ?</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="modal-button text-center">
+                                                        <ul>
+                                                            <li>
+                                                                <form action="{{route('banner.destroy',$banner->id)}}" method="POST">
+                                                                    @csrf
+                                                                    <button type="submit" class="btn btn-danger">Confirm</button>
+                                                                </form>
+
+                                                            </li>
+                                                            <li>
+                                                                <button type="button" class="btn btn-success" data-dismiss="modal">Cancel</button>
+                                                            </li>
+                                                        </ul>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>
+                        @if ($banners->count()==0)
+                            <div class="alert alert-info">
+                                No banner added please add a banner first
+                            </div>
+                        @endif
+
+                    </div>
+
+                    {{-- Table Content end --}}
+                    <!-- d-flex -->
+                </div>
+                <!-- d-flex -->
+            </div>
+            <!-- card -->
+
+            {{-- page body content end --}}
+        </div>
+    </div>
+</div>
+@endsection
