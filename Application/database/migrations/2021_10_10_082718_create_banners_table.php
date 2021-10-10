f043c49aeea55e4092006a0b73325870bba69ca1
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-10 14:43:53 +0600

    Model and Migration created #20

---
+++ Application/database/migrations/2021_10_10_082718_create_banners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBannersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->text('banner_text')->unique();
+            $table->text('banner_image');
+            $table->string('status')->default(1)->comment('1 = Active, 2 = Inactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+}
