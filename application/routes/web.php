d5d183d5cbbe1758b102e896b844289d27d1d4c6
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-10 18:15:42 +0600

    Laravel Breeze Installed #45

--- application/routes/web.php
+++ application/routes/web.php
@@ -16,43 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin'], function(){
-    Route::get('/dashboard', 'App\Http\Controllers\Backend\DashboardController@dashboard') ->name('admin.dashboard');
-     //this routes are for branch management
-    Route::group(['prefix' => '/branch'], function(){
-        Route::get('/manage','App\Http\Controllers\Backend\BranchController@index')->name('branch.manage');
-        Route::get('/create','App\Http\Controllers\Backend\BranchController@create')->name('branch.create');
-        Route::post('/store','App\Http\Controllers\Backend\BranchController@store')->name('branch.store');
-        Route::get('/edit/{id}','App\Http\Controllers\Backend\BranchController@edit')->name('branch.edit');
-        Route::post('/update/{id}','App\Http\Controllers\Backend\BranchController@update')->name('branch.update');
-        Route::post('/destroy/{id}','App\Http\Controllers\Backend\BranchController@destroy')->name('branch.destroy');
-    });
-    //this routes are for employee management
-    Route::group(['prefix' => '/employee'], function(){
-        Route::get('/manage','App\Http\Controllers\Backend\EmployeeController@index')->name('employee.manage');
-        Route::get('/create','App\Http\Controllers\Backend\EmployeeController@create')->name('employee.create');
-        Route::post('/store','App\Http\Controllers\Backend\EmployeeController@store')->name('employee.store');
-        Route::get('/edit/{id}','App\Http\Controllers\Backend\EmployeeController@edit')->name('employee.edit');
-        Route::post('/update/{id}','App\Http\Controllers\Backend\EmployeeController@update')->name('employee.update');
-        Route::post('/destroy/{id}','App\Http\Controllers\Backend\EmployeeController@destroy')->name('employee.destroy');
-    });
-    //this routes are for coupon management
-    Route::group(['prefix' => '/coupon'], function(){
-        Route::get('/manage','App\Http\Controllers\Backend\CouponController@index')->name('coupon.manage');
-        Route::get('/create','App\Http\Controllers\Backend\CouponController@create')->name('coupon.create');
-        Route::post('/store','App\Http\Controllers\Backend\CouponController@store')->name('coupon.store');
-        Route::get('/edit/{id}','App\Http\Controllers\Backend\CouponController@edit')->name('coupon.edit');
-        Route::post('/update/{id}','App\Http\Controllers\Backend\CouponController@update')->name('coupon.update');
-        Route::post('/destroy/{id}','App\Http\Controllers\Backend\CouponController@destroy')->name('coupon.destroy');
-    });
-    //this routes are for coupon management
-    Route::group(['prefix' => '/banner'], function(){
-        Route::get('/manage','App\Http\Controllers\Backend\BannerController@index')->name('banner.manage');
-        Route::get('/create','App\Http\Controllers\Backend\BannerController@create')->name('banner.create');
-        Route::post('/store','App\Http\Controllers\Backend\BannerController@store')->name('banner.store');
-        Route::get('/edit/{id}','App\Http\Controllers\Backend\BannerController@edit')->name('banner.edit');
-        Route::post('/update/{id}','App\Http\Controllers\Backend\BannerController@update')->name('banner.update');
-        Route::post('/destroy/{id}','App\Http\Controllers\Backend\BannerController@destroy')->name('banner.destroy');
-    });
 
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
