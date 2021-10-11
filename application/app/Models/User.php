aaf5fc47f684c7635435ea5f11a7dd9652d644ac
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-11 12:44:35 +0600

    model updated #44

--- application/app/Models/User.php
+++ application/app/Models/User.php
@@ -20,6 +20,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'phone',
+        'address',
+        'image',
+        'role'
     ];
 
     /**
