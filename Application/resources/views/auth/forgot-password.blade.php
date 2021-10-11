dad435021645484f44755394f057958894709c3e
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-11 12:13:48 +0600

    forgot password page updated #44

--- Application/resources/views/auth/forgot-password.blade.php
+++ Application/resources/views/auth/forgot-password.blade.php
@@ -1,36 +1,26 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.adminlayouts')
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-        </div>
+@section('body')
+<div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
 
+    <div class="login-wrapper wd-300 wd-xs-400 pd-25 pd-xs-40 bg-white rounded shadow-base">
+      <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span> bracket <span class="tx-info">plus</span> <span class="tx-normal">]</span></div>
+      <div class="tx-center mg-b-40">The Admin Template For Perfectionist</div>
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
-
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
+         <!-- Email Address -->
+          <div class="form-group">
+             <x-label for="email" :value="__('Email')" />
+              <x-input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus />
+           </div>
+          <div class="form-group">
+           <button type="submit" class="btn btn-info btn-block">Reset Password</button>
+          </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+    </div><!-- login-wrapper -->
+  </div><!-- d-flex -->
+@endsection
