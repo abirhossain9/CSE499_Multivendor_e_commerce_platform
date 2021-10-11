e782d97e511fbfd649cb595bf68f32b1d475d160
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-11 12:15:01 +0600

    password reset page #44

--- Application/resources/views/auth/reset-password.blade.php
+++ Application/resources/views/auth/reset-password.blade.php
@@ -1,48 +1,45 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.adminlayouts')
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+@section('body')
+<div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
 
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
+    <div class="login-wrapper wd-300 wd-xs-400 pd-25 pd-xs-40 bg-white rounded shadow-base">
+      <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span> bracket <span class="tx-info">plus</span> <span class="tx-normal">]</span></div>
+      <div class="tx-center mg-b-40">The Admin Template For Perfectionist</div>
+      <!-- Validation Errors -->
+      <x-auth-validation-errors class="mb-4" :errors="$errors" />
+      <form method="POST" action="{{ route('password.update') }}">
+        @csrf
 
-            <!-- Password Reset Token -->
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+        <!-- Password Reset Token -->
+        <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+        <!-- Email Address -->
+        <div class="form-group">
+            <x-label for="email" :value="__('Email')" />
+            <x-input id="email" class="form-control" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+        </div>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
-            </div>
+        <!-- Password -->
+        <div class="form-group">
+            <x-label for="password" :value="__('Password')" />
+            <x-input id="password" class="form-control" type="password" name="password" required />
+        </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+        <!-- Confirm Password -->
+        <div class="form-group">
+            <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
-            </div>
+            <x-input id="password_confirmation" class="form-control"
+                                type="password"
+                                name="password_confirmation" required />
+        </div>
 
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+        <div class="class="form-group">
+            <button type="submit" class="btn btn-info btn-block">Reset Password</button>
+        </div>
+    </form>
+    </div>
+</div>
 
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                    type="password"
-                                    name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+@endsection
