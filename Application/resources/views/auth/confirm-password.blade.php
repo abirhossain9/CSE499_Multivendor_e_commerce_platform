c3feb8108db7204644f4fe64f2e567b0a125ec2c
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-11 12:13:22 +0600

    confirm password page updated #44

--- Application/resources/views/auth/confirm-password.blade.php
+++ Application/resources/views/auth/confirm-password.blade.php
@@ -1,36 +1,32 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.adminlayouts')
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+@section('body')
+<div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
+
+    <div class="login-wrapper wd-300 wd-xs-400 pd-25 pd-xs-40 bg-white rounded shadow-base">
+      <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span> bracket <span class="tx-info">plus</span> <span class="tx-normal">]</span></div>
+      <div class="tx-center mg-b-40">The Admin Template For Perfectionist</div>
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+       <!-- Validation Errors -->
+       <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+       <form method="POST" action="{{ route('password.confirm') }}">
+        @csrf
+        <!-- Password -->
+        <div class="form-group">
+            <x-label for="password" :value="__('Password')" />
+
+            <x-input id="password" class="form-control"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
         </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-info btn-block">Confirm</button>
+      </div>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('password.confirm') }}">
-            @csrf
-
-            <!-- Password -->
-            <div>
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <div class="flex justify-end mt-4">
-                <x-button>
-                    {{ __('Confirm') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+ </form>
+    </div><!-- login-wrapper -->
+  </div><!-- d-flex -->
+@endsection
