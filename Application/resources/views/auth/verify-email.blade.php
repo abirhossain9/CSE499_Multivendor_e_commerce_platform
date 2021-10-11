5562273b145026a7bf6f069465f57d223ca1491b
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-10-11 12:15:23 +0600

    email verify page #44

--- Application/resources/views/auth/verify-email.blade.php
+++ Application/resources/views/auth/verify-email.blade.php
@@ -1,39 +1,31 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.adminlayouts')
+@section('body')
+<div class="d-flex align-items-center justify-content-center bg-br-primary ht-100v">
+    <div class="login-wrapper wd-300 wd-xs-400 pd-25 pd-xs-40 bg-white rounded shadow-base">
+      <div class="signin-logo tx-center tx-28 tx-bold tx-inverse"><span class="tx-normal">[</span> bracket <span class="tx-info">plus</span> <span class="tx-normal">]</span></div>
+      <div class="tx-center mg-b-40">The Admin Template For Perfectionist</div>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-        </div>
-
-        @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+      @if (session('status') == 'verification-link-sent')
+      <div class="mb-4 font-medium text-sm text-green-600">
+          {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+      </div>
+     @endif
+    <div class="mt-4 flex items-center justify-between">
+        <form method="POST" action="{{ route('verification.send') }}">
+            @csrf
+            <div>
+              <div class="form-group">
+                  <button type="submit" class="btn btn-info btn-block">Resend Verification Email</button>
+              </div>
+           </div>
+          </form>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <div class="form-group">
+                <button type="submit" class="btn btn-info btn-block">Log Out</button>
             </div>
-        @endif
-
-        <div class="mt-4 flex items-center justify-between">
-            <form method="POST" action="{{ route('verification.send') }}">
-                @csrf
-
-                <div>
-                    <x-button>
-                        {{ __('Resend Verification Email') }}
-                    </x-button>
-                </div>
-            </form>
-
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-
-                <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    {{ __('Log Out') }}
-                </button>
-            </form>
-        </div>
-    </x-auth-card>
-</x-guest-layout>
+        </form>
+    </div><!-- login-wrapper -->
+  </div><!-- d-flex -->
+</div>
+@endsection
