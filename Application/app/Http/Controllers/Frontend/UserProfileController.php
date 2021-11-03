696cd2a4f941dd80fc999b7c2a6b7c04b4997c61
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-11-03 20:34:59 +0600

    Controller updated #72

--- Application/app/Http/Controllers/Frontend/UserProfileController.php
+++ Application/app/Http/Controllers/Frontend/UserProfileController.php
@@ -12,7 +12,66 @@ use Image;
 class UserProfileController extends Controller
 {
 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::orderBy('id','asc')->get();
+        return view('backend.pages.user.manage',compact('users'));
 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('backend.pages.employee.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        if(!empty($user)){
+            return view('backend.pages.user.edit',compact('user'));
+        }
+        else{
+            return redirect()->route('user.manage');
+        }
+    }
     /**
      * Update the specified resource in storage.
      *
@@ -38,7 +97,13 @@ class UserProfileController extends Controller
             $user->image = $img;
         }
         $user->save();
-        return redirect()->route('user.dashboard');
+        if($user->role ==3){
+             return redirect()->route('user.dashboard');
+        }
+        elseif($user->role ==2){
+             return redirect()->route('vendor.dashboard');
+        }
+
 
     }
 
