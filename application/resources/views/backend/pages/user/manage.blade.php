a3aff9456bd4f42c35e84786ff1b06ea8f350669
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-11-04 08:57:21 +0600

    user manage page for admin panel #72
    user manage page for admin panel

---
+++ application/resources/views/backend/pages/user/manage.blade.php
@@ -0,0 +1,148 @@
+@extends('backend.layout.template') @section('body')
+
+<div class="br-pagetitle">
+    <i class="icon ion-ios-home-outline"></i>
+    <div>
+        <h4>Manage All Users</h4>
+        <p class="mg-b-0">Manage All Users From Here.</p>
+    </div>
+</div>
+
+<div class="br-pagebody">
+    <div class="row">
+        <div class="col-lg-12">
+            {{-- page body content start --}}
+
+            <div class="card bd-0 shadow-base">
+                <div class="d-md-flex justify-content-between pd-25">
+
+                    {{-- Table Content start --}}
+                    <div class="bd bd-gray-300 rounded table-responsive">
+                        <table class="table table-bordered table-striped table-hover table-custom">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">#sl.</th>
+                                    <th scope="col">Image</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Verification</th>
+                                    <th scope="col">Phone</th>
+                                    <th scope="col">Address</th>
+                                    <th scope="col">Role</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php $i = 0; @endphp
+                                @foreach($users as $user)
+                                @php $i++; @endphp
+
+                                <tr>
+                                    <th scope="row">{{$i;}}</th>
+                                    <td>@if ($user->profile_pic==NULL)
+                                         <img src="{{asset('backend/img/users/default.jpg')}}" alt="" width="40">
+                                    @else
+                                         <img src="{{asset('backend/img/users/'.$user->image)}}" alt="" width="40">
+
+                                    @endif</td>
+                                    <td>{{$user->name}}</td>
+                                    <td>{{$user->email}}</td>
+                                    <td>
+                                        @if ($user->email_verified_at==null)
+                                        <span class="badge badge-info">Not Verified</span>
+                                        @else
+                                        <span class="badge badge-info">Verified</span>
+                                        @endif
+                                    </td>
+                                    <td>{{$user->phone}}</td>
+                                    <td>{{$user->address}}</td>
+                                    <td>
+                                        @if ($user->role==2)
+                                        <span class="badge badge-info">Vendor</span>
+                                        @elseif ($user->role==3)
+                                        <span class="badge badge-info">Customer</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if ($user->status==1)
+                                        <span class="badge badge-success">active</span>
+                                        @elseif ($user->status==2)
+                                        <span class="badge badge-danger">inactive</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <ul class="custom-action">
+                                            <li>
+                                                <a href="{{route('user.edit',$user->id)}}">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="" data-toggle="modal" data-target="#user{{$user->id}}">
+                                                    <i class="fa fa-trash "></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </td>
+                                    <!-- Modal -->
+                                    <div
+                                        class="modal fade"
+                                        id="user{{$user->id}}"
+                                        tabindex="-1"
+                                        role="dialog"
+                                        aria-labelledby="exampleModalCenterTitle"
+                                        aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLongTitle">Delte This user ?</h5>
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="modal-button text-center">
+                                                        <ul>
+                                                            <li>
+                                                                <form action="{{route('user.destroy',$user->id)}}" method="POST">
+                                                                    @csrf
+                                                                    <button type="submit" class="btn btn-danger">Confirm</button>
+                                                                </form>
+
+                                                            </li>
+                                                            <li>
+                                                                <button type="button" class="btn btn-success" data-dismiss="modal">Cancel</button>
+                                                            </li>
+                                                        </ul>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>
+                        @if ($users->count()==0)
+                            <div class="alert alert-info">
+                                No users added please add a users first
+                            </div>
+                        @endif
+
+                    </div>
+
+                    {{-- Table Content end --}}
+                    <!-- d-flex -->
+                </div>
+                <!-- d-flex -->
+            </div>
+            <!-- card -->
+
+            {{-- page body content end --}}
+        </div>
+    </div>
+</div>
+@endsection
