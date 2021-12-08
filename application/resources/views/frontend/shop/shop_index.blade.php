f11ffaf2190ab3c6aa0fbb3080028dfa427a1a51
Emamul Hassan <emamul.hassan@northsouth.edu>
2021-12-08 22:59:17 +0600

    updated scripts
    updated scripts

--- application/resources/views/frontend/shop/shop_index.blade.php
+++ application/resources/views/frontend/shop/shop_index.blade.php
@@ -386,12 +386,9 @@
 </div>
 
 <!-- Bootstrap JS CDN -->
-<script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
-    integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF"
-    crossorigin="anonymous"></script>
-<!-- jquery JS File -->
-<script src="{{ asset('frontend/js/jquery-3.2.1.min.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
 <!-- Isotope JS File -->
 <script src="{{ asset('frontend/plugins/Isotope/isotope.pkgd.min.js') }}"></script>
 <!-- Owl Carousel JS File -->
