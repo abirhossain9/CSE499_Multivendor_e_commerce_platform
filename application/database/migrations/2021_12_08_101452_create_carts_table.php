ff7823b3a9bdbb04aefebcffc6bf910fb38cb8de
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-12-10 19:53:31 +0600

    order table deleted and carts updated

--- application/database/migrations/2021_12_08_101452_create_carts_table.php
+++ application/database/migrations/2021_12_08_101452_create_carts_table.php
@@ -17,15 +17,13 @@ class CreateCartsTable extends Migration
             $table->increments('id');
             $table->unsignedInteger('product_id');
             $table->unsignedInteger('user_id');
-            $table->unsignedInteger('order_id')->nullable();
-            $table->string('ip_address')->nullable();
-            $table->integer('product_quantity')->default(1);
-
-            $table->timestamps();
-
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            // $table->unsignedInteger('order_id')->nullable();
+            // $table->string('ip_address')->nullable();
+            // $table->integer('product_quantity')->default(1);
+            // $table->timestamps();
+            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            // $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            // $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
 
         });
     }
